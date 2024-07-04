e188548b8589a05c9fcc88cc6f0cf039ecd93e6c
Paolo Siroko <paolosiroko@gmail.com>
2024-07-04 21:42:31 +0300

    create journal model and serializer

--- backend/service/urls.py
+++ backend/service/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import JournalEntryViewSet
 from . import views
+
+router = DefaultRouter()
+router.register(r'entries', JournalEntryViewSet, basename='journalentry')
+
 urlpatterns = [
-    path('', views.api, name="api"),
+    path('', include(router.urls)),
 ]
--- backend/service/views.py
+++ backend/service/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import viewsets, permissions
+from .models import JournalEntry
+from .serializers import JournalEntrySerializer
+from rest_framework import status
+from rest_framework.response import Response
+
 
 # Create your views here.
-def api(request):
-    return HttpResponse("Journal App")
+class JournalEntryViewSet(viewsets.ModelViewSet):
+    queryset = JournalEntry.objects.all()
+    serializer_class = JournalEntrySerializer
+
